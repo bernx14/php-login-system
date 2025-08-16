1d93a0628413d9469644021710f52ef682b9d4b4
Bernie Acdal <77830144+bernx14@users.noreply.github.com>
2025-08-16 18:49:40 +0800

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,27 @@
+<?php
+// logout.php
+// This script destroys the user's session, effectively logging them out.
+
+// We must start the session to be able to access and destroy it.
+session_start();
+
+// Unset all of the session variables.
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Finally, destroy the session.
+session_destroy();
+
+// Redirect to the login page
+header("Location: login.php");
+exit();
+?>
